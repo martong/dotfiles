9f70cd30b792f04b34e615551e37e5d441e37de0
Gabor Marton <gabor.marton@ericsson.com>
2019-11-21 16:15:50 +0100

    [fzf][zsh] Add C-p for tmux

--- home/.fzf_tmux_hist.zsh
+++ home/.fzf_tmux_hist.zsh
@@ -17,3 +17,23 @@ fzf-tmux-hist-widget() {
 }
 zle     -N   fzf-tmux-hist-widget
 bindkey '^F' fzf-tmux-hist-widget
+
+__fseltmuxhist_words() {
+  local cmd='tmux capture-pane -pt "$target-pane" | tr "[:space:]" "\n" | tr "[:punct:]" "\n"'
+  setopt localoptions pipefail 2> /dev/null
+  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) --tac -m "$@" | while read item; do
+    echo -n "${(q)item} "
+  done
+  local ret=$?
+  echo
+  return $ret
+}
+
+fzf-tmux-hist-words-widget() {
+  LBUFFER="${LBUFFER}$(__fseltmuxhist_words)"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
+zle     -N   fzf-tmux-hist-words-widget
+bindkey '^P' fzf-tmux-hist-words-widget
