aee5ed0db4e1e7ac1269bf3d399dc9eb41be4be2
Gabor Marton <gabor.marton@ericsson.com>
2019-12-02 14:38:21 +0100

    Make .fzf_tmux and .fzf_git better

--- home/.fzf_git_files.zsh
+++ home/.fzf_git_files.zsh
@@ -1,22 +1,37 @@
-# fgst - pick files from `git status -s`
+__lastInLbuffer() {
+  local array=("${(@s/ /)LBUFFER}")
+  echo "${array[-1]}"
+}
+
+__lbufferWithoutLast() {
+  local array=("${(@s/ /)LBUFFER}")
+  unset "array[-1]"
+  echo "${array[@]}"
+}
+
 is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
 
+# fgst - pick files from `git status -s`
 __fgst() {
   # "Nothing to see here, move along"
-  is_in_git_repo || return
+  is_in_git_repo || return 1
 
-  local cmd="${FZF_CTRL_T_COMMAND:-"command git status -s"}"
-
-  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) -m "$@" | while read -r item; do
-    echo "$item" | awk '{print $2}'
-  done
-  echo
+  setopt localoptions pipefail 2> /dev/null
+  git status -s | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) -1 --query="$(__lastInLbuffer)" | awk '{print $2}'
+  local ret=$?
+  return "$ret"
 }
 
+# Select one file from git status.
 fzf-git-files-widget() {
-  LBUFFER="${LBUFFER}$(__fgst)"
+  local selected
+  selected=$(__fgst)
+  # The user successfully selected one element so update the LBUFFER.
+  if [ $? -eq 0 ]; then
+    LBUFFER="$(__lbufferWithoutLast)$selected"
+  fi
   local ret=$?
   zle reset-prompt
   return $ret
